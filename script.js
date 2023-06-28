694841718254bf592d3f9da17e8be554314939d9
Diana-Sh1 <dianaa15052015@gmail.com>
2023-06-28 15:44:18 +0300

    test

--- script.js
+++ script.js
@@ -14,12 +14,44 @@ function longestPrefix(arr) {
 const arr = ['abc123', 'abcd123', 'abcde123', 'abcdefg123']
 console.log(longestPrefix(arr));
 
-//метод работы с массивом every().
+////////////метод работы с массивом every()/some(). Можно использовать несколько проверок
 const array = [4,5,6,7,8]
 let b = array.every(item => {
     if (item > 3) return true;
 })
-console.log(b);
+console.log(b);// true
+// ВСЕ ли совершеннолетние люди в списке. Если есть хоть 1 несовершеннолетний, то вернется false
+const c = [
+    {name: 'John', age: 13},
+    {name: 'Ann', age: 22},
+    {name: 'Ien', age: 10},
+]
+let d = c.every(item => {
+    if (item.age >= 18) return true;
+})
+    console.log(d);// false
+
+
+///////////Метод работы с массивом reduce().
+//Найти максимальное значение в массиве.
+    let e = array.reduce((accum,item)=> {
+    if (item > accum) {
+        accum = item}
+    return accum;
+})
+    console.log(e);
+//Найти и получить только все id из строк массива
+let arrayString = [
+    {"id": 55, "city": "Moscow"},
+    {"id": 45, "city": "Abr"},
+    {"id": 34, "city": "Oka"},
+]
+let g = arrayString.reduce((accum, item) => {
+    accum.push(item.id);
+    return accum
+},[]);
+console.log(g);
+
 
 
 
